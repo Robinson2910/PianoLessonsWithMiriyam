ea86ded3877deee779bb301f103be18f761a9638
Robinson <manuvelrobinson@gmail.com>
2025-05-23 03:04:48 +0530

    Added Map

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import "../css/about.css";
+
 function About() {
   return (
     <section className="section section--about">
@@ -7,50 +8,43 @@ function About() {
           <h1 className="about-heading margin-bottom-small">
             So, who am I?
           </h1>
-          <img
-            src="./img/contact-image.jpg"
-            alt="miryam"
-            className="about-img-hidden-l"
-          />
-          <p className="about-description margin-bottom-small">
-            Hi there! I’m Miriyam Joseline, a
-            certified piano teacher with over 14
-            years of experience, and I’m thrilled
-            to share the joy of music with you.
-          </p>
-          <p className="about-description margin-bottom-small">
-            Music is a beautiful gift that can
-            brighten your days, spark creativity,
-            and bring a sense of peace like
-            nothing else. My lessons are all about
-            helping you build strong skills while
-            making sure you have fun and feel
-            inspired every step of the way.
-          </p>
-          <p className="about-description margin-bottom-small">
-            Whether you’re just picking up the
-            keys for the first time or looking to
-            take your playing to new heights, I’m
-            here to guide you, cheer you on, and
-            celebrate every little victory. Ready
-            to start your musical adventure? Let’s
-            make every note count together!
-          </p>
+          <div className="about-text-container">
+            <p className="about-description margin-bottom-small">
+              Hi there! I'm Miriyam Joseline, a
+              certified piano teacher with over 14
+              years of experience, and I'm
+              thrilled to share the joy of music
+              with you.
+            </p>
+            <p className="about-description margin-bottom-small">
+              Music is a beautiful gift that can
+              brighten your days, spark
+              creativity, and bring a sense of
+              peace like nothing else. My lessons
+              are all about helping you build
+              strong skills while making sure you
+              have fun and feel inspired every
+              step of the way.
+            </p>
+            <p className="about-description margin-bottom-small">
+              Whether you're just picking up the
+              keys for the first time or looking
+              to take your playing to new heights,
+              I'm here to guide you, cheer you on,
+              and celebrate every little victory.
+              Ready to start your musical
+              adventure? Let's make every note
+              count together!
+            </p>
+          </div>
           <a
-            className="cta-btn "
+            className="cta-btn"
             href="https://wa.me/+917338785700"
             target="_blank"
             rel="noopener noreferrer"
           >
             JOIN ME
           </a>
-        </div>
-        <div className="about-img-box">
-          <img
-            src="./img/contact-image.jpg"
-            alt="miryam"
-            className="about-img"
-          />
         </div>
       </div>
     </section>
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -48,10 +48,17 @@ function ContactUs() {
           </div>
         </div>
         <div className="contact-img-box">
-          <img
-            src="./img/contact-image.jpg"
-            alt="miriyam teaching student"
-            className="contact-img"
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.5620476595!2d80.22083967381192!3d13.063525412858748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267a630fc5eff%3A0x4906f83b30cc56f4!2sPiano%20Lessons%20With%20Miriyam!5e0!3m2!1sen!2sin!4v1747949349982!5m2!1sen!2sin"
+            style={{
+              width: "100%",
+              height: "100%",
+              border: 0,
+            }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Piano Lessons With Miriyam"
           />
         </div>
       </div>
