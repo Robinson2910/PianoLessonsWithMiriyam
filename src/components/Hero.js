9492b4da0b39fe546f540c8318560e6ceee6de1f
Robinson <manuvelrobinson@gmail.com>
2025-05-23 14:19:11 +0530

    heading update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
         <h1 className="hero-heading margin-bottom-vsmall">
           Hey there, and welcome!
         </h1>
-        <p className="para margin-bottom-vsmall">
+        <p className="para ">
           So glad you're here! If you've been
           dreaming about learning the piano or
           picking it up again - you're in the
@@ -17,13 +17,11 @@ function Hero() {
           something you truly enjoy. We'll take it
           step by step, build your confidence, and
           celebrate every little victory along the
-          way
+          way. Whether you're 6 or 60, there's a
+          seat at the piano waiting for you. Ready
+          to press that first key? Let's begin!
         </p>
-        <p className="para margin-bottom-vsmall">
-          Whether you're 6 or 60, there's a seat
-          at the piano waiting for you. Ready to
-          press that first key? Let's begin!
-        </p>
+
         <div className="button-box">
           <a
             className="cta-btn"
